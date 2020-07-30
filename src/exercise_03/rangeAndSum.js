c4f837dea651e10a2cf21bff75ed35cfd5b9846b
twodogbeta <qijinhaoup@163.com>
2020-07-31 00:03:13 +0800

    finish exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,17 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const answer = new Array();
+  if (start <= end) {
+    for (let i = start; i < end; i += 1) {
+      answer.push(i);
+    }
+  } else {
+    for (let i = start; i > end; i -= 1) {
+      answer.push(i);
+    }
+  }
+  return answer;
 }
 
 export function sum(...numbers) {
@@ -17,6 +26,9 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let answer = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    answer += numbers[i];
+  }
+  return answer;
 }
