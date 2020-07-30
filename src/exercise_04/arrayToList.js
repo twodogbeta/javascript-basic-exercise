541535bb8b15ddba1c5ad9abcbf296ee3b5a92ee
twodogbeta <qijinhaoup@163.com>
2020-07-31 00:08:06 +0800

    finish exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,28 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === null || array === undefined) throw new Error('Creating list from undefined array');
+  if (array.length === 0) throw new Error('Creating list from empty array');
+  class LinkedNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
 
-  throw new Error('Please delete this line and implement the function');
+    const LinkedList = [];
+    for (let i = array.length - 1 ; i >= 0; i--) {
+      const item = {};
+      if ((i + 1) !== array.length) {
+        item.value = array[i];
+        item.next = LinkedList[i + 1];
+      } else {
+        item.value = array[i];
+        item.next = null;
+      }
+      LinkedList[i] = item;
+    }
+  
+    return { value: LinkedList[0].value, next: LinkedList[0].next };
+  }
+
 }
