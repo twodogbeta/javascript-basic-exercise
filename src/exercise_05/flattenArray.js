ec0cbc25c494087195adabd7a5182143a45a9bd5
twodogbeta <qijinhaoup@163.com>
2020-07-31 00:11:17 +0800

    finish exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,16 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const res = [];
+  array.forEach((cur) => {
+    if (cur instanceof Array) {
+      res.push(...cur);
+    } else {
+      res.push(cur);
+    }
+  });
+  return res;
 }
